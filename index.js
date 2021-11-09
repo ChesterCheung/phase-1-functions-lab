1cc15778daf402a6db02fb99f04e20c2f5f0d6e2
Chester <cheung319@gmail.com>
2021-11-09 17:57:41 -0500

    commit tree 4a63faa88384d40eb2c76519981cb76b77ca2ec1

--- index.js
+++ index.js
@@ -22,14 +22,17 @@ function distanceFromHqInFeet(block){
 }
 
 function calculatesFarePrice(start, destination){
-    if (distanceTravelledInFeet <= 400){
-        return (distanceFromHqInFeet * 0)
-    }else if (distanceFromHqInFeet < 2000)
-        return (distanceFromHqInFeet * 0.2)
-    else if (distanceFromHqInFeet > 2000)
+    const distance = distanceTravelledInFeet(start, destination)
+
+    if (distance <= 400){
+        return (distance * 0)
+    }else if (distance > 400 && distance < 2000){
+        return (distance - 400) * 0.02
+    }else if (distance > 2000 && distance < 2500){
         return 25
-    else
+    }else{
         return "cannot travel that far"
+    }
 }
 
         
